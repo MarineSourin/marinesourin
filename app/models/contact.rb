c6f491f59e5749d78993c7542787dbbd3b2cd82e
Marine Sourin <marinesourin@live.fr>
2019-04-01 11:28:08 +0200

    MailForm

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,14 @@
-class Contact < ApplicationRecord
+class Contact < MailForm::Base
+  attribute :name, :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :message,   :validate => true
+  attribute :nickname,  :captcha  => true
+
+  def headers
+    {
+      :subject => "Contact Form",
+      :to => "marinesourin@live.fr",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :cvs, only: :index
   resources :portfolios, only: :index
-  resources :contacts, only: :index
+  resources :contacts, only: [:new, :create]
 end
