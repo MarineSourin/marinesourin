9312d2588d719e194d908fc8e32cefec580bc46c
Marine Sourin <marinesourin@live.fr>
2019-05-28 15:15:33 +0200

    contact form started

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,19 +2,19 @@ class ContactsController < ApplicationController
   def index
   end
 
-  # def new
-  #   @contact = Contact.new
-  # end
+  def new
+    @contact = Contact.new
+  end
 
-  # def create
-  #   @contact = Contact.new(params[:contact])
-  #   @contact.request = request
-  #   if @contact.deliver
-  #     flash.now[:error] = nil
-  #     redirect_to root_path, notice: 'Message sent successfully'
-  #   else
-  #     flash.now[:error] = 'Cannot send message'
-  #     render :new
-  #   end
-  # end
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:error] = nil
+      render :index, notice: 'Message sent successfully'
+    else
+      flash.now[:error] = 'Cannot send message'
+      render :index
+    end
+  end
 end
