7e05c035c85ba35cd7e9923cb4ae7911047ddc16
Marine Sourin <marinesourin@live.fr>
2019-05-28 17:00:49 +0200

    contact form working

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < MailForm::Base
   attribute :name, :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :email,     :validate => /\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b/i
   attribute :message,   :validate => true
   attribute :nickname,  :captcha  => true
 
